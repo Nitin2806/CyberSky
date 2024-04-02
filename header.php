85f43949ae68f79c96842ee0c4b5ade038775f99
13navin <144178630+13navin@users.noreply.github.com>
2024-04-02 08:34:10 -0400

    add files

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css" />
+        <script src="script.js"></script>
+        <title>Cybersky</title>
+    </head>
+
+    <body>
+        <nav>
+            <div class="container">
+                <img src="logo.jpeg">
+                <ul>          
+                    <li><a href="index.php">Dashboard</a></li>
+                    <li><a href="create.php">Create Order</a></li>
+                </ul>
+            </div>
+        </nav>
